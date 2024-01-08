011b6f8ed43de79a4a22fb5b497be70ac28cac1c
Anthony Schneider <tonyschneider3@gmail.com>
2024-01-07 21:54:37 -0600

    v1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,47 @@
-"use client";
+"use client"
 
+import { useState } from "react"
 import Link from "next/link"
-import PasswordStrengthBar from 'react-password-strength-bar';
+import PasswordStrengthBar from "react-password-strength-bar"
+
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
 
 export default function IndexPage() {
+  const [password, setPassword] = useState("")
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
+      <div className="flex flex-col gap-4">
+        <Input
+          // type={showPassword ? "text" : "password"}
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter password"
+        />
+        <PasswordStrengthBar password={password} />
+        <div className="items-top flex space-x-2">
+          <Checkbox
+            id="displayPasswordCheckbox"
+            onChange={() => setShowPassword(!showPassword)}
+            disabled
+            checked={true}
+          >
+            Show Password
+          </Checkbox>
+          <div className="grid gap-1.5 leading-none">
+            <label
+              htmlFor="displayPasswordCheckbox"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Show Password
+            </label>
+          </div>
+        </div>
       </div>
     </section>
   )
