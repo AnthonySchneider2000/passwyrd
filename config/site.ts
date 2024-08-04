98196846547038c46ccda61f74f816bf7a83e237
Anthony Schneider <tonyschneider3@gmail.com>
2024-08-03 19:22:27 -0500

    Metadata: updated site name in site config

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Passwyrd",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "A simple, quick, and open-source password strength checker.",
   mainNav: [
     {
       title: "Home",
